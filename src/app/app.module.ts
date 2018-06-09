680e767e96d9b2759ad063da6ac78b122ffc7890
Mark Franciose <mark.franciose@gmail.com>
2018-06-09 06:41:54 -0500

    Static Pages - Add Acronyms Page
    - Add http, rxjs method, acro page to app module
    - Create JSON file w/ acronym info
    - Create basic acronym page
    - Add acroynm module & html link to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,33 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule, NavController } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
+
+// import 'rxjs/add/operator/map'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Page_2Page } from '../pages/page-2/page-2';
-
+import { AcronymsPage } from '../pages/acronyms/acronyms';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     Page_2Page,
+  	AcronymsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+	IonicModule.forRoot(MyApp),
+	HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     Page_2Page,
+		AcronymsPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { NavController, PopoverController } from 'ionic-angular';
 import { Page_2Page } from '../page-2/page-2';
 
+import { AcronymsPage } from '../acronyms/acronyms';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  acronymsPage = AcronymsPage;
 
   constructor(public navCtrl: NavController,public popCtrl: PopoverController) {
 
