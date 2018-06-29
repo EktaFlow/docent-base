029cf35b026b8ab483dfd1da72fdc2feae14ee55
Noah Adolphson <adolp059@umn.edu>
2018-06-29 15:09:48 -0500

    Finished second page styling - navCtrl modified on first page to transfer some values - navCtrl modified on second page to receive those values - Second page view finished

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController, PopoverController } from 'ionic-angular';
+
 import { Page_2Page } from '../page-2/page-2';
+import { AcronymsPage } from '../acronyms/acronyms';
+import { DefinitionsPage } from '../definitions/definitions';
+import { HelpmenuComponent } from '../../components/helpmenu/helpmenu';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,public popCtrl: PopoverController) {
+  acronymsPage = AcronymsPage;
+  definitionsPage = DefinitionsPage;
+
+  members = [];
+
+  constructor(public navCtrl: NavController,public popOver: PopoverController) {
 
   }
-  addMember(){
 
+  showPopover(myEvent) {
+    var popoverClick = this.popOver.create(HelpmenuComponent, {}, {cssClass: 'help-menu'});
+      popoverClick.present({
+        ev: myEvent
+      });
+    }	
+  
+  addMember(nameIn:string,roleIn:string){
+    var newMember = {name: nameIn, role: roleIn};
+    this.members.push(newMember);
   }
   removeMember(){
-
+    this.members.pop()
   }
-  page_2(){
-    this.navCtrl.push(Page_2Page);
+  page_2(targetMRL){
+    this.navCtrl.push(Page_2Page,{
+      data: targetMRL
+    });
   }
 }
--- src/pages/page-2/page-2.ts
+++ src/pages/page-2/page-2.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
+import { PopoverComponent } from '../../components/popover/popover';
 
 /**
  * Generated class for the Page_2Page page.
@@ -14,14 +15,24 @@ import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-an
   templateUrl: 'page-2.html',
 })
 export class Page_2Page {
-
+  targetMRL = null;
   slectorResponse = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl:PopoverController) {
+    this.targetMRL = navParams.get('data');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Page_2Page');
   }
 
+  pageSelect(event){
+    let popover = this.popoverCtrl.create(PopoverComponent);
+    popover.present({
+      ev: event
+     });
+  }
+  back(){
+    this.navCtrl.pop();
+  }
 }
