4ddc4de7435681653710cc3edc3b09e4f4f31db1
Noah Adolphson <adolp059@umn.edu>
2018-07-17 08:46:00 -0500

    Removing unwanted components for branch

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, PopoverController } from 'ionic-angular';
 
-import { Page_2Page } from '../page-2/page-2';
-import { AcronymsPage } from '../acronyms/acronyms';
-import { DefinitionsPage } from '../definitions/definitions';
-import { HelpmenuComponent } from '../../components/helpmenu/helpmenu';
 import { QuestionsPage } from '../questions/questions';
 
 @Component({
@@ -14,21 +10,11 @@ import { QuestionsPage } from '../questions/questions';
 })
 export class HomePage {
 
-  acronymsPage = AcronymsPage;
-  definitionsPage = DefinitionsPage;
-
   members = [];
 
   constructor(public navCtrl: NavController,public popOver: PopoverController) {
 
-  }
-
-  showPopover(myEvent) {
-    var popoverClick = this.popOver.create(HelpmenuComponent, {}, {cssClass: 'help-menu'});
-      popoverClick.present({
-        ev: myEvent
-      });
-    }	
+  }	
   
   addMember(nameIn:string,roleIn:string){
     var newMember = {name: nameIn, role: roleIn};
