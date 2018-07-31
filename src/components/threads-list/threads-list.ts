edb18fb4d07317e08fe97b07a2f49012e8e503b9
mark franciose <mark.franciose@gmail.com>
2018-07-31 17:32:40 -0500

    Finish commits to move to master
    - comment out secrets in constants
    - commit incomplete threads list component

--- src/components/threads-list/threads-list.ts
+++ src/components/threads-list/threads-list.ts
@@ -17,10 +17,13 @@ export class ThreadsListComponent {
 	threads: any;
 	chosen = [];
 	emitter: any;
+	threadsSelected: any;
 
   constructor(public navParams: NavParams, public viewController:  ViewController) {
 		this.threads = this.navParams.get("allThreads");
 		this.emitter = this.navParams.data.emitter;
+		this.threadsSelected = this.navParams.data.threadsSelected;
+		console.log(this.threadsSelected);
   }
 
 	dismiss(item) {
@@ -29,8 +32,8 @@ export class ThreadsListComponent {
 
 	emit(item) {
 		this.emitter.emit(item);
+		if (this.threadsSelected.includes(item.index)) {this.threadsSelected = this.threadsSelected.filter(a => a !== item.index)}
+		else { this.threadsSelected.push(item.index)} 
 	}
-
-
 
 }
