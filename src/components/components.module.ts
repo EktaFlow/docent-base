16bfcf5973121214271f494c7eecddef13344d97
mark franciose <mark.franciose@gmail.com>
2018-07-30 20:49:05 -0500

    asd

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ViewsComponent } from './views/views';
+@NgModule({
+	declarations: [ViewsComponent],
+	imports: [],
+	exports: [ViewsComponent]
+})
+export class ComponentsModule {}
