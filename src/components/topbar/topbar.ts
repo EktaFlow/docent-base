34c2904238b6acbf36cd52985c720b17e2a3e414
mark franciose <mark.franciose@gmail.com>
2018-08-04 22:07:36 -0500

    Make presentation tweaks
    - Add tooltip css and text to question mark icons on Home Page.
    - Add ability to toggle scope on TopBar
    - Correct date formatting on TopBar

--- src/components/topbar/topbar.ts
+++ src/components/topbar/topbar.ts
@@ -6,6 +6,19 @@ import { LoginPage }    from "../../pages/login/login";
 import { AuthService } from "../../services/auth.service";
 import { HelpmenuComponent } from "../helpmenu/helpmenu";
 
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
+var assessmentQuery = gql`
+query assessment($_id: String!) {
+	assessment(_id: $_id) {
+		scope
+		targetMRL
+		targetDate
+	}
+}
+`
+
 @Component({
   selector: 'topbar',
   templateUrl: 'topbar.html'
@@ -15,6 +28,10 @@ export class TopbarComponent {
 
 	public loginPage		= LoginPage;
 	public registerPage = RegisterPage;
+	public scope: any;
+	public targetMRL: any;
+	public targetDate: any;
+	public scopeSelected: any;
 	@Input() public assessmentId: any;
 	// the question info is only relevant for the questions page. whereas the assessments info is relevant for all the pages.
 	@Input() private mainTitle: any;
@@ -22,11 +39,31 @@ export class TopbarComponent {
 
 
 constructor( public popOver: PopoverController,
-						 public auth: AuthService,
-						 public navCtrl: NavController ) { }
+						 public auth:    AuthService,
+						 public navCtrl: NavController,
+						 private apollo: Apollo ) { }
 
 	ngOnInit() {
-		console.log(this.assessmentId);
+		this.assessmentId ? this.getAssessmentData() : null;
+	}
+
+	toggleScopeSelected() {
+		this.scopeSelected = !this.scopeSelected;
+	}
+
+	getAssessmentData() {
+		this.apollo.watchQuery<any>({
+			query: assessmentQuery,
+			variables: {
+				_id: this.assessmentId
+			}
+		}).valueChanges
+			.subscribe( ({data, loading}) => {
+				console.log(data);
+				this.scope		  = data.assessment.scope;
+				this.targetMRL  = data.assessment.targetMRL;
+				this.targetDate = data.assessment.targetDate;
+			}); 
 	}
 
   presentViewsPop(event){
