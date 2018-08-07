d452af14ff858b9200d7163e12e807a6c8022d65
mark franciose <mark.franciose@gmail.com>
2018-08-07 13:50:42 -0500

    Implement Basic Assessment Export
    - include file saver lib
    - export assessment info as text file.
    - typescript casting nonsense

--- src/components/views/views.ts
+++ src/components/views/views.ts
@@ -9,8 +9,53 @@ import { NotapplicablePage } from '../../pages/notapplicable/notapplicable';
 import { SkippedquestionsPage } from '../../pages/skippedquestions/skippedquestions';
 import { ActionitemsPage } from '../../pages/actionitems/actionitems';
 import { QuestionsPage } from "../../pages/questions/questions";
+import { saveAs } from "file-saver/FileSaver";
+
+
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
 
 import {AssessmentslistComponent} from "../assessmentslist/assessmentslist";
+
+var assessmentQuery = gql`
+query assessment($_id: String) 
+{
+ assessment(_id: $_id)  {
+	questions{
+	  currentAnswer
+    skipped
+		questionId
+    threadName
+    subThreadName
+    mrLevel
+		questionId
+		questionText
+		objectiveEvidence
+		assumptionsYes
+		notesYes
+		who
+		when
+		technical
+		cost
+		schedule
+		what
+	reason
+		assumptionsNo
+		notesNo
+		documentation
+		assumptionsNA
+		notesNA
+  }
+	targetMRL
+	currentMRL
+	levelSwitching
+	name
+	files {
+		url
+	}
+}
+}
+`
 
 @Component({
   selector: 'views',
@@ -26,20 +71,37 @@ export class ViewsComponent {
 	assessments: any;
 	assessmentId: any;
   
-  constructor( public navCtrl: NavController, public popOver: PopoverController, public navParams: NavParams, public viewCtrl: ViewController) {
+	constructor(  public navCtrl: NavController, 
+								public popOver: PopoverController, 
+								public navParams: NavParams, 
+								public viewCtrl: ViewController, 
+								private apollo: Apollo ) {
 		this.assessmentId = navParams.data.assessmentId;
   }
+
+
+		handleSave() {
+	this.apollo.watchQuery<any>({
+			query: assessmentQuery,
+			fetchPolicy: "network-only",
+			variables: {_id: this.assessmentId}
+		})
+			.valueChanges
+			.subscribe( ({data, loading}) => {
+				var title = data.assessment.name;
+				title ? null : title = "untitled"	
+				var assessment = JSON.stringify(data);	
+				saveAs(new Blob([assessment], { type: "text/plain" }), title + ".mra")
+			})
+		}
 
 		handleSkipped() {
 			this.navCtrl.push(SkippedquestionsPage, {assessmentId: this.assessmentId})
 		}
-
 		handleNa() {
 			this.navCtrl.push(NotapplicablePage, {assessmentId: this.assessmentId});
 		}
-
 		handleContinue = () => this.navCtrl.push(QuestionsPage, { data: this.assessmentId});
-
 		handleActions = () => this.navCtrl.push(ActionitemsPage, {assessmentId: this.assessmentId});
 		handleReview = () => this.navCtrl.push(ReviewPage, {assessmentId: this.assessmentId});
 		handleNavigate = () => this.navCtrl.push(NavigatePage, {assessmentId: this.assessmentId});
