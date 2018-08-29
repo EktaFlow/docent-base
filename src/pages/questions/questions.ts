8098ad3de8d6a73cafb4e650ec8bc8771b5c0a7e
mark franciose <mark.franciose@gmail.com>
2018-08-29 14:34:43 -0500

    checkpoint - qa-able nav & fixes

--- src/pages/questions/questions.ts
+++ src/pages/questions/questions.ts
@@ -73,15 +73,10 @@ query question($questionId: Int, $assessmentId: String) {
   templateUrl: 'questions.html',
 })
 
-
 export class QuestionsPage {
 
-	/// Object to hold the different vars for the follow-up questions
 	private vals = {};
-
 	assessmentId: any;
-	assessmentSubscription: any;
-
 	private questionId: any; 
 	files = [];
 	private allQuestions;
@@ -91,209 +86,32 @@ export class QuestionsPage {
 	private surveyQuestions;
 	private levelSwitching: any;
 
-	constructor(public navCtrl: NavController, 
-              public navParams: NavParams, 
+	constructor(public navCtrl:            NavController, 
+              public navParams:          NavParams, 
 							private popoverController: PopoverController, 
-							private apollo: Apollo ) {
+							private apollo:            Apollo ) {
 
 		// QUESTION - SAVE THIS IN LOCAL MEMORY? 
 		this.referringQuestionId = navParams.data.questionId;
 		this.assessmentId = navParams.data.data;
   }
-
+	
   /////////////////////////// useful functions ///////////////////////
 	// return a question by its questionId
 	getQuestion = (id) => this.allQuestions[id - 1]
 
 	allSubthreadQuestions(question = this.currentQuestion) {
 		return this.allQuestions
-		           .filter(q => q.subThreadName == question.subThreadName)
+		           .filter(q => q.subThreadName == question.subThreadName);
 	}
 
 	allSubthreadLevelQuestions(question = this.currentQuestion) {
-		return this.allSubthreadLevelQuestions()
+		return this.allQuestions
+		           .filter(q => q.subThreadName == question.subThreadName)
 		           .filter(q => q.mrLevel == question.mrLevel );
 	}
-
-	/////////////////////////// popover creator(s) /////////////////////
-	showFileUpload(event) {
-	let myEmitter = new EventEmitter<any>();
-			myEmitter.subscribe( v =>  {
-				this.files.push(v);
-			});
-
-	var fileUploadPopover = this.popoverController.create(FileUploadPopoverComponent, 
-			{
-				emitter: myEmitter, 
-				questionId: this.questionId, 
-				assessmentId: this.assessmentId 
-			}, 
-			{	cssClass: "upload-popover"});
-		fileUploadPopover.present();
-	}
-
-	async handleNextPageClick() {
-		this.setValues();
-		// move to the new question
-		this.levelSwitching ? this.handleLevelSwitching() : this.nextQuestion(1)
-
-		// reset the values to the new ones.
-		this.vals = this.currentQuestion;
-	}
-
-	async	handlePreviousPageClick() {
-		this.setValues();
-		this.nextQuestion(-1);
-		this.vals = this.currentQuestion;
-	}
-
-	setValues() {
-		this.updateAssessment(this.vals)
-	}
-
-	// refactor this down
-	async updateAssessment(values) {
-
-		var values = Object.assign({}, values)
-		values = this.filterQuestionVals(values);
-		var a = this.allQuestions.find(a => a.questionId == this.currentQuestion.questionId);
-		var old = this.allQuestions.map( q => Object.assign({}, q));
-		var newer = old[this.currentQuestion.questionId - 1];
-
-		for (let a in values) {
-			newer[a] = values[a];
-		}
-
-		var temp = JSON.parse(JSON.stringify(this.allQuestions));
-		temp.splice(this.currentQuestion.questionId - 1, 1, newer);
-		this.allQuestions = temp;
-
-		await this.apollo.mutate({
-			mutation: updateAssessmentQuery,
-			variables: {
-				_id:				this.assessmentId,
-				questionId: Number(this.currentQuestion.questionId),
-				updates:		values
-			}
-		}).subscribe(data => null);
-	}
-
-	nextQuestion(way) {
-		var { questionId } = this.currentQuestion;	
-
-		if (!this.surveyQuestions.includes(this.currentQuestion.questionId)) {
-			alert("what is the order when a rando question gets added in?");
-			this.currentQuestion = this.getQuestion(this.surveyQuestions[0]);
-		} 
-		else {
-			console.log(this.surveyQuestions);
-			var place = this.surveyQuestions.indexOf(questionId) + way;
-			var newQuestion = this.surveyQuestions[place];
-			this.currentQuestion = this.getQuestion(newQuestion);
-		}
-	}
-
-	nextUnansweredQuestion() {
-			// the humble while loop
-			var place = 0;
-			while (this.currentQuestion.currentAnswer) {
-				place += 1;
-				this.currentQuestion = this.getQuestion(this.surveyQuestions[place]);
-			}
-	}
-
-	handleLevelSwitching() {
-				if ( !this.threadAnswered()) {
-					console.log("not end of thread");
-					this.nextQuestion(1);
-				}
-				// make sure this encompasses all non-switching scenarios.
-				else if ( this.threadPassed() ) {
-					console.log("threadpassed");
-					this.nextUnansweredQuestion();
-					// launch some type of thread passed UI change.
-					//this.nextQuestion(1);
-        } 
-				else {
-					alert("You have failed this subthread, you will be shown questions from this subthread at the next lowest level");
-					this.launchLevelSwitchModal();
-				}
-	}
-
-	// Does every question within the currentQuestion's subthread and MRL have an answer? 
-	threadAnswered() {
-		var {mrLevel, subThreadName} = this.currentQuestion;
-		
-		return this.allQuestions.filter(q => q.mrLevel == mrLevel && q.subThreadName == subThreadName)
-		                 .every(q => ["Yes", "No", "N/A"].includes(q.currentAnswer))
-
-	}
-	
-	checkNextQuestion() {
-		var ok = this.surveyQuestions.indexOf(this.currentQuestion.questionId);
-		var next = this.surveyQuestions[ok + 1]
-		return this.getQuestion(next);
-	}
-
-	// the only way to 'fail' a subthread is to have a no answer. 
-	threadPassed() {
-		return !this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.subThreadName)
-					        .filter(q => q.mrLevel == this.currentQuestion.mrLevel)
-									.some( q => q.currentAnswer == "No")
-	}
-
-	launchLevelSwitchModal() {
-		this.addLowerMRL();
-	}
-
-	addLowerMRL() {
-    var nextLowest = this.allQuestions
-                       .filter(q => q.subThreadName == this.currentQuestion.subThreadName)		
-											 .filter(q => q.mrLevel == this.currentQuestion.mrLevel - 1)
-											 .map(q => q.questionId);
-
- 	  var newSurvey = [...nextLowest, ...this.surveyQuestions].sort( (a,b) => a - b)
-	  this.surveyQuestions = newSurvey;
-
-	  this.currentQuestion = this.getQuestion(nextLowest[0]);
-	}
-
-
-	// this function takes an arr of questions as assessment sub-documents 
-	// and formats them to be in the surveyJs format.
-	// this is the format that survey JS is expecting, so we don't 
-	// mess with the structure.
-	mapToSurveyJS(questions) {
-		return questions.map( question => {
-			return {
-				name: question.threadName,
-				elements: [{
-					type: "dropdown",
-					name: question.subThreadName,
-					title: question.questionText,
-					choices: [
-						"Yes",
-						"No",
-						"N/A"
-					]
-				}]
-			};
-		});
-	}
-
-	setInstanceVariables(assessment) {
-		this.levelSwitching = assessment.levelSwitching	
-	}
-
-	setSurveyQuestions() {
-		var { targetMRL,
-					allQuestions } = this;
-
-    return allQuestions.filter( q => q.mrLevel == targetMRL )
-		                   .map( q => q.questionId);
-	}
-
-	// What data do we actually need to store in instance vars?
+	/////////////////////////////////////////////////////////////////////
+	// INIT && related function 
   ngOnInit() {
 	// if we don't already have a loaded assessment.
 		this.apollo.watchQuery<any>({
@@ -317,22 +135,227 @@ export class QuestionsPage {
 
   }
 
+	setInstanceVariables(assessment) {
+		this.levelSwitching = assessment.levelSwitching	
+	}
+
+	setSurveyQuestions() {
+		var { targetMRL,
+					allQuestions } = this;
+
+    return allQuestions.filter( q => q.mrLevel == targetMRL )
+		                   .map( q => q.questionId);
+	}
+
+
 	determineCurrentQuestion() {
 		var { referringQuestionId, 
 					currentQuestion,
           getQuestion } = this;
 
 		if (referringQuestionId ) {
-			currentQuestion = getQuestion(referringQuestionId);
+			this.currentQuestion = getQuestion(referringQuestionId);
 		}
 		else {
 			var noAnswer = this.surveyQuestions.find( qId => {
 				return getQuestion(qId).currentAnswer == null
 			})
-
 			this.currentQuestion = getQuestion(noAnswer);
 		}
 	}
+
+	////////////////// CLICK HANDLERS //////////////////////////////////
+	/////////////////////////// popover creator(s) /////////////////////
+	showFileUpload() {
+			let myEmitter = new EventEmitter<any>();
+				myEmitter.subscribe( v =>  {
+				this.files.push(v);
+			});
+
+			this.popoverController
+				.create(FileUploadPopoverComponent, 
+					{
+						emitter: myEmitter, 
+						questionId: this.questionId, 
+						assessmentId: this.assessmentId 
+					}, 
+					{	cssClass: "upload-popover"})
+				.present();
+	}
+
+	///////////////////////// next / prev / etc /////////////////////////////
+	async handleNextPageClick() {
+		this.setValues();
+		this.levelSwitching ? this.handleLevelSwitching() : this.moveCurrentQuestion(1)
+		this.vals = this.currentQuestion;
+	}
+
+	async	handlePreviousPageClick() {
+		this.setValues();
+		this.moveCurrentQuestion(-1);
+		this.vals = this.currentQuestion;
+	}
+
+	/////////////////////////////////////////////////////////////////////////
+	///////////// FUNCTIONS DEALING WITH VALUE SETTING /////////////////////
+
+	///// any modification of the inputs needed to be used in the assessment
+	///// update function
+	setValues() {
+		var values = Object.assign({}, this.vals)
+		values.currentAnswer === null ? values.currentAnswer = "skipped" : null
+		values = this.filterQuestionVals(values);
+
+		this.updateAssessment(values)
+	}
+
+	// refactor this down
+	async updateAssessment(values) {
+		var a = this.allQuestions.find(a => a.questionId == this.currentQuestion.questionId);
+		var old = this.allQuestions.map( q => Object.assign({}, q));
+		var newer = old[this.currentQuestion.questionId - 1];
+
+		for (let a in values) {
+			newer[a] = values[a];
+		}
+
+		var temp = JSON.parse(JSON.stringify(this.allQuestions));
+		temp.splice(this.currentQuestion.questionId - 1, 1, newer);
+		this.allQuestions = temp;
+
+		await this.apollo.mutate({
+			mutation: updateAssessmentQuery,
+			variables: {
+				_id:				this.assessmentId,
+				questionId: Number(this.currentQuestion.questionId),
+				updates:		values
+			}
+		}).subscribe(data => null);
+	}
+
+	moveCurrentQuestion(way) {
+		var { questionId } = this.currentQuestion;	
+
+		if (!this.surveyQuestions.includes(this.currentQuestion.questionId)) {
+			alert("what is the order when a rando question gets added in?");
+			this.currentQuestion = this.getQuestion(this.surveyQuestions[0]);
+		} 
+		else {
+			var place = this.surveyQuestions.indexOf(questionId) + way;
+			var newQuestion = this.surveyQuestions[place];
+			this.currentQuestion = this.getQuestion(newQuestion);
+		}
+	}
+
+	nextUnansweredQuestion() {
+			// the humble while loop
+			var place = 0;
+			while (this.currentQuestion.currentAnswer) {
+				place += 1;
+				this.currentQuestion = this.getQuestion(this.surveyQuestions[place]);
+			}
+			// if all questions are answered? 
+	}
+
+	//////////////////////////////// 
+	////////// LEVEL SWITCHING ONLY functions
+	///////////////////////////////
+
+	handleLevelSwitching() {
+				if ( !this.threadAnswered()) {
+					this.moveCurrentQuestion(1);
+				}
+				// make sure this encompasses all non-switching scenarios.
+				else if ( this.threadPassed() ) {
+					this.nextUnansweredQuestion();
+					// launch some type of thread passed UI change?? 
+        } 
+				else if ( this.hasFloorLevel() ) {
+					this.moveCurrentQuestion(1);
+				}
+				else {
+					alert("You have failed this subthread, you will be shown questions from this subthread at the next lowest level");
+					this.launchLevelSwitchModal();
+				}
+	}
+
+	// FUNCTIONS to handle the different branches on level-switching 
+	// Does every question within the currentQuestion's subthread and MRL have an answer? 
+	threadAnswered() {
+		var {mrLevel, subThreadName} = this.currentQuestion;
+		
+		return this.allQuestions.filter(q => q.mrLevel == mrLevel && q.subThreadName == subThreadName)
+		                 .every(q => ["Yes", "No", "N/A"].includes(q.currentAnswer))
+
+	}
+	
+	// the only way to 'fail' a subthread is to have a no answer. 
+	threadPassed() {
+		return !this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.subThreadName)
+					        .filter(q => q.mrLevel == this.currentQuestion.mrLevel)
+									.some( q => q.currentAnswer == "No")
+	}
+
+	// If a subthread is complete at a lower level, we've found the floor, so no need to bounce
+	// user back again.
+	// this needs some more testin
+	hasFloorLevel() {
+	  // start with current Question;
+		var {currentQuestion} = this;
+		var currentSubthread = this.allSubthreadQuestions();
+		var floor = false;
+
+		currentSubthread.forEach(q => {
+			var level = this.allSubthreadLevelQuestions(q)
+			console.log(level);
+			level.every(ques => ["Yes", "Skipped", "skipped", "N/A"].includes(ques.currentAnswer) ) ? floor = true : null
+		});
+
+		console.log(floor);
+		return floor;
+	}
+
+	launchLevelSwitchModal() {
+		this.addLowerMRL();
+	}
+
+	addLowerMRL() {
+    var nextLowest = this.allQuestions
+                       .filter(q => q.subThreadName == this.currentQuestion.subThreadName)		
+											 .filter(q => q.mrLevel == this.currentQuestion.mrLevel - 1)
+											 .map(q => q.questionId);
+
+ 	  var newSurvey = [...nextLowest, ...this.surveyQuestions].sort( (a,b) => a - b)
+	  this.surveyQuestions = newSurvey;
+
+	  this.currentQuestion = this.getQuestion(nextLowest[0]);
+	}
+
+	///////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////
+
+	// this function takes an arr of questions as assessment sub-documents 
+	// and formats them to be in the surveyJs format.
+	// this is the format that survey JS is expecting, so we don't 
+	// mess with the structure.
+	mapToSurveyJS(questions) {
+		return questions.map( question => {
+			return {
+				name: question.threadName,
+				elements: [{
+					type: "dropdown",
+					name: question.subThreadName,
+					title: question.questionText,
+					choices: [
+						"Yes",
+						"No",
+						"N/A"
+					]
+				}]
+			};
+		});
+	}
+
 
 	filterQuestionVals(question) {
 		// better way to do this.
