c1ac84d10d3d3c22af09eade735fb7aa23b68b87
Dominique Goncalves <domstar1595@yahoo.com>
2018-09-05 11:58:18 -0500

    pop up for subthreads box and styling

---
+++ src/components/subthread-popup/subthread-popup.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the SubthreadPopupComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'subthread-popup',
+  templateUrl: 'subthread-popup.html'
+})
+export class SubthreadPopupComponent {
+
+  constructor() {
+  }
+
+}
