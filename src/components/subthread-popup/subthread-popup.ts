e01056e752508ac62766502ccb63b3f5bdbb361a
Dominique Goncalves <domstar1595@yahoo.com>
2018-09-06 13:24:49 -0500

    add functionality and should work (i think). i do not currently have access to app because of auth service, waiting for acess.

--- src/components/subthread-popup/subthread-popup.ts
+++ src/components/subthread-popup/subthread-popup.ts
@@ -1,4 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
+import { QuestionsPage } from '../../pages/questions/questions';
+
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
+var assessmentQuery = gql`
+query assessment($_id: String!) {
+	assessment(_id: $_id) {
+		questions{
+      subThreadName
+      questionText
+      questionId
+			mrLevel
+    }
+	}
+}
+`
 
 /**
  * Generated class for the SubthreadPopupComponent component.
@@ -12,7 +30,39 @@ import { Component } from '@angular/core';
 })
 export class SubthreadPopupComponent {
 
-  constructor() {
-  }
+	public targetMRL: any;
+	public questions: any;
+	public currentQuestions: any;
+	@Input() public assessmentId: any;
+	@Input() private subTitle: any;
+
+  constructor(private apollo: 			 Apollo,
+							 public navCtrl: 			 NavController,
+							 public navParams: 		 NavParams) {}
+
+	 ngOnInit() {
+	 	this.assessmentId ? this.getAssessmentData() : null;
+	 }
+
+	 getAssessmentData() {
+	 	this.apollo.watchQuery<any>({
+	 		query: assessmentQuery,
+	 		variables: {
+	 			_id: this.assessmentId
+	 		}
+	 	}).valueChanges
+	 		.subscribe( ({data, loading}) => {
+	 			console.log(data);
+	 			this.targetMRL  = data.assessment.targetMRL;
+				this.questions = data.assessment.questions;
+	 		});
+	 }
+
+  navToQuestion(questionId) {
+		this.navCtrl.push(QuestionsPage, {
+			data: 			this.assessmentId,
+			questionId: questionId
+		});
+	}
 
 }
