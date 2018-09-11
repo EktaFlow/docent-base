22b5fab500c2d26774ecb4eb0673d55cbe714d0d
Dominique Goncalves <domstar1595@yahoo.com>
2018-09-11 11:32:28 -0500

    added assessment query to user dash.ts but not successfully getting info - http failure network error

--- src/pages/user-dashboard/user-dashboard.ts
+++ src/pages/user-dashboard/user-dashboard.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthService } from "../../services/auth.service";
+import {Subscription} from "rxjs";
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
 
+var assessmentQuery = gql`
+query assessment($userId: String) {
+	assessment(userId: $userId) {
+	   scope
+     targetMRL
+     targetDate
+     levelSwitching
+     deskbookVersion
+     location
+     name
 
-/**
- * Generated class for the UserDashboardPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+	}
+}
+`
 
 @IonicPage()
 @Component({
@@ -17,24 +27,39 @@ import { AuthService } from "../../services/auth.service";
 })
 export class UserDashboardPage {
 
-  constructor(public navCtrl: NavController,
-                    public navParams: NavParams) {}
-                    //private auth: AuthService
+
 
   public fakeUser: any = {
     name: "Jane Doe",
     email: "janedoe@docent.co",
-    organization: "Docent"
+    organization: "Docent",
+    id: "test_dash"
   };
+  assessments: any;
+  loading: boolean;
+  private querySubscription: Subscription;
 
+  constructor(public navCtrl: NavController,
+                    public navParams: NavParams,
+                    private apollo: Apollo) {}
+                    //private auth: AuthService
 
+  async ngOnInit() {
+    var userId = this.fakeUser.id;
+    console.log(this.fakeUser.id);
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: assessmentQuery,
+      variables: {
+        userId
+      }
+    })
+    .valueChanges
+    .subscribe(({data, loading}) => {
+      this.loading = loading;
+      this.assessments = data.assessments;
+    });
 
-
-
-
-  // ngOnInit() {
-  //
-  // }
+  }
 
 
 
