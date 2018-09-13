8ffaf7926989d632c704a6988a8fe2fbf0273ba5
mark franciose <mark.franciose@gmail.com>
2018-09-13 18:20:43 -0500

    add calls to hit assessment sharing route(s)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -63,6 +63,7 @@ var createAssessmentMutation = gql`
      $name: String
 		 $levelSwitching: Boolean
 		 $userId: String
+		 $teamMembers: [String]
    ) {
      createAssessment(
        threads:    $threads,
@@ -74,6 +75,7 @@ var createAssessmentMutation = gql`
        targetDate: $targetDate,
        deskbookVersion: $deskbookVersion,
 			 name: $name,
+			 teamMembers: $teamMembers,
 			 levelSwitching: $levelSwitching
      ) {
           _id
@@ -137,6 +139,7 @@ export class HomePage {
 		alert("You must be a registered Docent user to begin an assessment");
 	}
 
+
 	// use form??
 	createAssessment(event) {
 	console.log("fire");
@@ -144,27 +147,56 @@ export class HomePage {
 		// TODO: make this into a real function with a front end modal <01-08-18, mpf> //
 		var values = this.assForm;
 		values.threads = this.threadsSelected;
-    var fakeTeamMember = {email: "cool@cool.net", role: "it's cool"}
+    var fakeTeamMember = { email: "cool@cool.net", role: "it's cool"}
 
+		console.log(this.assForm.teamMembers);
+		var teamMembers = this.assForm.teamMembers.map(a => a.email)
+		console.log(teamMembers);
 
 		this.apollo.mutate({
 				mutation:		createAssessmentMutation,
 				variables:	{
-				threads:				values.threads,
-				location: "cool",
-				targetMRL:				6,
-				name: "cool",
-				levelSwitching:		false,
-				deskBookVersion:	"2017",
-				scope: "aaaaaaaaaaaaa",
-        // teamMembers: [fakeTeamMember],
-				// targetDate: new Date,
-				userId: "dev"
+					threads:					values.threads,
+					location:					"cool",
+					targetMRL:				6,
+					name:							"cool",
+					levelSwitching:		false,
+					deskBookVersion:	"2017",
+					scope:						"aaaaaaaaaaaaa",
+					teamMembers,
+					// targetDate: new Date,
+					userId: "dev"
 				}
 		})
 			.subscribe(({data}) => {
-					this.page_2(data.createAssessment._id);
+					var assessmentId = data.createAssessment._id;
+					this.sendEmailsToTeamMembers(assessmentId);
+					this.page_2(assessmentId);
 			});
+	}
+
+	async sendEmailsToTeamMembers(assessmentId) {
+		console.log("sending");
+		var teamMembers = this.assForm.teamMembers.map(mem => mem.email);
+		console.log(teamMembers);
+
+		// move this to constants when we decide it's home.
+		var url = "http://localhost:4002/share";
+
+		// this makes sense in auth b/c we probably do want some user checking here, right?
+		fetch(url, {
+			method: "POST",
+			headers: {
+	      'Accept': 'application/json',
+	      'Content-Type': 'application/json'
+				},
+			body: JSON.stringify({
+				recipients: teamMembers,
+				assessmentId
+			})
+		})
+		.then(a => console.log("okok"))
+		.catch(e => console.error(e));
 	}
 
 	async ngOnInit() {
@@ -252,7 +284,6 @@ export class HomePage {
   removeMember(){
     this.members.pop();
     this.assForm.teamMembers.pop();
-
   }
 
   questions(date,val,loc){
