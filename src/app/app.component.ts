3d60babfc78dd4ee5821060b4de91de0c11a38b7
Dominique Goncalves <domstar1595@yahoo.com>
2018-10-15 16:57:31 -0500

    re-align pages on mobile while using mobile browser / mobile app pages were being covered up by the top bar because it has a larger height, so all pages are readjusted to be fixed. also on criteria & navigate pages the buttons for the 'toolbar' (expand all & filter) were wonky so they are now fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+// import { Keyboard } from '@ionic-native/keyboard';
 
 import { HomePage } from '../pages/home/home';
 import { UserDashboardPage } from "../pages/user-dashboard/user-dashboard";
@@ -15,7 +16,7 @@ export class MyApp {
 
 	constructor(platform:     Platform,
               statusBar:    StatusBar,
-              splashScreen: SplashScreen, 
+              splashScreen: SplashScreen,
               auth:         AuthService) {
     platform.ready().then(() => {
 		if (auth.isLoggedIn()) {
@@ -26,9 +27,9 @@ export class MyApp {
 		}
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      // keyboard.hideKeyboardAccessoryBar(false);
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 }
-
