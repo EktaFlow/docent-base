09e747295648f811a7d2c7ac2802e348043f0e14
James Barkley <james.barkley@gmail.com>
2019-01-06 11:03:41 -0600

    - basic thread/subthread spitting out into (improperly formatted) table

--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -12,59 +12,28 @@ import gql from "graphql-tag";
 var assessmentQuery = gql`
 query assessment($_id: String) {
 	assessment(_id: $_id) {
-	questions {
-		mrLevel
-		questionText
-		threadName
-		subThreadName
-		currentAnswer
-		questionId
-                answers {
-		when
-		who
-		risk
-		what
-		reason
-		assumptionsNo
-                notesNo
-                answer
-                # technical
-		# schedule
-                # cost
-                }
-	}
-	files {
-		name
-		questionId
-		url
-	}
-	}
-}
 
-var assessmentQuery = gql`
-query assessment($_id: String) {
-	assessment(_id: $_id) {
-	targetMRL
-	targetDate
-	location
-        scope
-	questions {
-		questionId
-		mrLevel
-		questionText
-		threadName
-		subThreadName
-		currentAnswer
-            answers {
-                risk
-            }
+                deskbookVersion
+                name
+		id
+		teamMembers
+	        targetMRL
+	        targetDate
+	        location
+                scope
+        	questions {
+        		mrLevel
+        		questionText
+        		threadName
+        		subThreadName
+        		currentAnswer
+        		questionId
+                        answers {
+	        	    likelihood
+                            consequence
+                        }
+    	        }
 	}
-	files {
-		name
-		questionId
-		url
-	}
-    }
 }
 `
 
@@ -77,11 +46,30 @@ query assessment($_id: String) {
 })
 export class SummaryPage {
 
-	allQuestions: any;
 	assessmentId: any;
+	allQuestions: any;
+        targetMRL: any;
+        targetDate: any;
+        location: any;
+        team: any;
+        survey: any;
+        surveyResults: any;
+        reviewResults = [];
+        response;
+        scope: any;
         questionSet: any;
-	showAll: any;
-	pageName: any = "Summary";
+        threads = {};
+        threadsArr = [];
+	pageName: any = "MRL Summary";
+
+
+
+	ionViewWillEnter() {
+            GoogleAnalytics.trackPage("MRLSummary");
+        }
+
+
+
 
 	constructor( private apollo: Apollo,
 							 public navCtrl: NavController,
@@ -91,9 +79,106 @@ export class SummaryPage {
 		this.assessmentId = navParams.data.assessmentId;
   }
 
+	ngOnInit() {
+		this.apollo.watchQuery({
+			query: assessmentQuery,
+			variables: {_id: this.assessmentId},
+			fetchPolicy: "network-only"
+			}).valueChanges
+			.subscribe(data => {
+                            var assessment = (<any>data.data).assessment;
+                            var questions = assessment.questions;
+                   
+                            console.log(assessment); 
+                            //set top-level items
+                            this.scope = assessment.scope;
+                            this.team = assessment.teamMembers;
+		            this.targetMRL = assessment.targetMRL;
+		            this.targetDate = assessment.targetDate;
+		            this.location = assessment.location;
+
+                            //create threads object with all the right data
+                            //also use arrays because that is how .html file can iterate 
+                            var threads = {};
+                            var threadsArr = [];
+                            questions.forEach(q => {
+                                if (threadsArr.indexOf(q.threadName) > -1) {
+                                } else {
+                                    if (q.threadName.length>0) {
+                                        threadsArr.push(q.threadName);
+                                    }
+                                }
+       
 
 
-  async ngOnInit() {
+                                //weed out troubling "" thread 
+                                if (q.threadName.length>0) {
+                                    if (!threads[q.threadName]) { 
+                                        threads[q.threadName] = {};
+                                    }
+                                    threads[q.threadName]["name"] = q.threadName; 
+
+                                    //setup subthreads
+                                    if (!threads[q.threadName][q.subThreadName]) { 
+                                        threads[q.threadName][q.subThreadName] = {};
+                                    }
+                                    threads[q.threadName][q.subThreadName] = q.subThreadName;
+
+                                    if (typeof threads[q.threadName]["subThreads"] !== 'undefined' && 
+                                        threads[q.threadName]["subThreads"].length > 0) {
+                                    } else {
+                                        threads[q.threadName]["subThreads"] = [];
+                                    }
+
+                                    //make sure we're building subThreads array without dups
+                                    if (threads[q.threadName]["subThreads"].indexOf(q.subThreadName) > -1) {
+                                    } else {
+                                        threads[q.threadName]["subThreads"].push(q.subThreadName);
+                                    }
+                                }
+
+                          });
+                          this.threads = threads;
+                          this.threadsArr = threadsArr;
+                          console.log("DEBUG A");
+                          console.log(this.threads);
+
+		});
+	}
+
+
+
+
+
+  public calculateRiskScore(likelihood, consequence) {
+    // preventing off by one errors, with nulls. 
+    // values should always be 1-5  
+    var riskMatrix = [
+      [ null ],
+      [ null, 1, 3,  5,  8,  12],
+      [ null, 2, 7,  11, 14, 17],
+      [ null, 4, 10, 15, 19, 21],
+      [ null, 6, 12, 18, 22, 24],
+      [ null, 9, 16, 20, 23, 25]
+    ];
+
+
+    if ( likelihood && consequence ) {
+      // value is the same as the index, b/c we put nulls in the matrix
+      var likelihoodIndex  = Number(likelihood);
+      var consequenceIndex = Number(consequence);   
+      
+      // var name = selectedBox.className.replace(/ selected/g, '')
+      // selectedBox.className = `${name} selected`;
+
+      return riskMatrix[likelihoodIndex][consequenceIndex]; 
+    } else {
+      return " ";
+    }
   }
 
+
+
 }
+
+
