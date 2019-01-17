a20ea592713eeab0a2863089b669da1853f0f4d2
Dominique Goncalves <domstar1595@yahoo.com>
2019-01-17 09:43:02 -0600

    cannot seem to fix date and adding in updateAssessment function tried to use formatDate function (similar to questions.ts formatDate function, but when i tried to save to this.assessment.targetDate, its a read only object but when i try to create a new assessment object (JSON.parse(JSON.stringify())) it freaks out saying invalid object so not sure what to do with that with the updateAssessment(), need to build out - there was no function so there was no way for it to even save before hand

--- src/pages/edit-assessment/edit-assessment.ts
+++ src/pages/edit-assessment/edit-assessment.ts
@@ -24,6 +24,7 @@ export class EditAssessmentPage {
       targetDate
       location
       name
+      levelSwitching
     }
   }
   `
@@ -31,6 +32,7 @@ export class EditAssessmentPage {
   private page: String = 'edit';
   private assessmentId: String;
   private assessment: any = {};
+  private assess: any;
 
   constructor(public navCtrl: NavController,
               private assessmentService: AssessmentService,
@@ -45,20 +47,57 @@ export class EditAssessmentPage {
   // things you can't --> the underlying schema
 
   async ngOnInit() {
-    // handle this when there is nope assId
+    // handle this when there is no assId
     this.assessmentId = await this.assessmentService.getCurrentAssessmentId();
     if (this.page == 'edit') {
       var assessment = await this.assessmentService.getAssessment(this.editQuery, this.assessmentId)
       assessment.subscribe( ({data}) => {
         this.assessment = data.assessment
+        this.assess = JSON.parse(JSON.stringify(this.assessment));
+        console.log(this.assess);
         console.log(this.assessment);
+        this.formattingValues();
+
       })
     }
+  }
+
+  //table this function for now
+  //trying to figure out how to actually save the formatting date so we can display it
+
+  formattingValues(){
+    this.assess.targetDate = this.formatDate();
+
+    // if (!this.assess.levelSwitching){
+    //   this.assess.levelSwitching = "No";
+    // }
+  }
+
+  formatDate() {
+    var date;
+    if (this.assessment.targetDate){
+      date = this.assessment.targetDate;
+    }
+
+  if (!date) {
+    return null;
+  } else {
+    return new Date(date)
+           .toISOString()
+           .slice(0,10);
+  }
   }
 
   closePopover(){
 		this.navCtrl.pop();
 	}
+
+  updateAssessment(e){
+    //need to access correct assessment
+    //then make sure we have a mutation for it in the back
+    //then call that query and send the information over
+    
+  }
 
 
 }
