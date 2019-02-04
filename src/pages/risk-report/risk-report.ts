ddbe61a618db3c4d52a1a59fb2f02f1d38ff57bb
Dominique Goncalves <domstar1595@yahoo.com>
2019-02-04 15:35:51 -0600

    grabbing correct information that will be displayed on risk report console logging it waiting for jim input to know which direction to move with styling

--- src/pages/risk-report/risk-report.ts
+++ src/pages/risk-report/risk-report.ts
@@ -1,6 +1,44 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TopbarComponent} from "../../components/topbar/topbar";
+import { TopbarComponent } from "../../components/topbar/topbar";
+import { GoogleAnalytics } from '../../application/helpers/GoogleAnalytics';
+
+
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
+
+var assessmentQuery = gql`
+query assessment($_id: String) {
+	assessment(_id: $_id) {
+          deskbookVersion
+          name
+		      teamMembers
+	        targetMRL
+	        targetDate
+	        location
+          scope
+        	questions {
+        		mrLevel
+        		questionText
+        		threadName
+        		subThreadName
+        		currentAnswer
+        		questionId
+            answers {
+                notesNo
+                notesYes
+                notesNA
+	        	    likelihood
+                consequence
+                greatestImpact
+                riskResponse
+                mmpSummary
+            }
+    	   }
+	  }
+}
+`
 
 /**
  * Generated class for the RiskReportPage page.
@@ -17,13 +55,30 @@ import { TopbarComponent} from "../../components/topbar/topbar";
 export class RiskReportPage {
 
   assessmentId: any;
+  pageName: any = "Detailed Risk Report";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo) {
     this.assessmentId = navParams.data.assessmentId;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RiskReportPage');
+  ionViewWillEnter() {
+    GoogleAnalytics.trackPage("RiskReport");
+  }
+
+  async ngOnInit(){
+    this.apollo.watchQuery({
+      query: assessmentQuery,
+      variables: {_id: this.assessmentId},
+      fetchPolicy: "network-only"
+      }).valueChanges
+      .subscribe(data => {
+        var assessment = (<any>data.data).assessment;
+        var questions = assessment.questions.filter(q => q.mrLevel == assessment.targetMRL);
+
+
+        console.log(assessment);
+        console.log(questions);
+      });
   }
 
 }
