5da3c71dee021b12b3e7c1742f508c8b95080878
Dominique Goncalves <domstar1595@yahoo.com>
2019-03-19 11:15:14 -0500

    remove action items still from mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
-      statusBar.styleBlackTranslucent();
+      // statusBar.styleBlackTranslucent();
       splashScreen.hide();
       keyboard.hideFormAccessoryBar(false);
       GoogleAnalytics.initialize();
