99bdaf17a4fa3c5a64dccfe0ff25be256e20b7e2
Dominique Goncalves <domstar1595@yahoo.com>
2019-09-19 11:28:02 -0500

    adding helper files

---
+++ src/app/helpers/helpers.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class Helpers {
+
+  constructor() {}
+
+  formatDate(date) {
+  console.log('hi');
+    return new Date(date)
+      .toISOString()
+      .slice(0,10);
+  }
+
+  public threadMap: any = {
+    "1": "Technology Maturity",
+    "2": "Technology & Industrial Base",
+    "3": "Design",
+    "4": "Cost & Funding",
+    "5": "Materials",
+    "6": "Process Capability & Control",
+    "7": "Quality Management",
+    "8": "Mfg Personnel",
+    "9": "Facilities",
+    "10": "Mfg Management"
+
+  };
+
+  public threadMap2016: any = {
+    "1": "Technology Maturity",
+    "2": "A. Technology & Industrial Base",
+    "3": "B. Design",
+    "4": "C. Cost & Funding",
+    "5": "D. Materials",
+    "6": "E. Process Capability & Control",
+    "7": "F. Quality Management",
+    "8": "G. Mfg Personnel",
+    "9": "H. Facilities",
+    "10": "I. Mfg Management"
+  };
+
+}
