c14cd74172fb5253853b5f0221889c3a76c7dda4
jcce <toshajo99@gmail.com>
2020-04-03 10:42:01 -0500

    refactored login into a command

--- cypress/integration/dod-resource-link.js
+++ cypress/integration/dod-resource-link.js
@@ -6,26 +6,11 @@ describe("validates DOD Resource link", function() {
     const baseUrl = Cypress.env("baseUrl");
 
     cy.visit(baseUrl);
-
-    // it is ok for the email to be visible in the Command Log
-    expect(email, "email was set").to.be.a("string").and.not.be.empty;
-    // but the password value should not be shown
-    if (typeof password !== "string" || !password) {
-      throw new Error("Missing password value, set using CYPRESS_password=...");
-    }
-
-    cy.get('input[name="emaial"]')
-      .type(email)
-      .should("have.value", email);
-    cy.get("[name=passwd]")
-      .type(password, { log: false })
-      .should(el$ => {
-        if (el$.val() !== password) {
-          throw new Error("Different value of typed password");
-        }
-      });
+    cy.login(email,password);
     cy.get(".button").click();
   });
+
+
 
   it("finds proper link for MRL Desktop", () => {
     cy.location("pathname").should("eq", "/");
