90cd5f48d3b1d7b0396c5422f7a13c5af0c8e03e
jcce <toshajo99@gmail.com>
2020-04-07 09:19:49 -0500

    Cleaned up topbar test, removed placeholder cypress envs

--- cypress/integration/top-bar.js
+++ cypress/integration/top-bar.js
@@ -1,8 +1,4 @@
-// auto complete from cypress
-/// <reference types="cypress" />
-
 describe("determine MR Level is same as Target MRL", function() {
-  const value = 9;
 
   beforeEach(() => {
     const email = Cypress.env("email");
@@ -31,50 +27,31 @@ describe("determine MR Level is same as Target MRL", function() {
   });
 
   it("clicks Start New", () => {
+    const value = 9;
     let targetMrl;
     let mrLevel;
 
     cy.get(".buttons > :nth-child(1) > .button-inner").click();
-
     // finds and fills assessment name field
-    cy.get("#assessment-name-input")
-      .type("test")
-      .should("have.value", "test");
-
+    cy.get("#assessment-name-input").type("test").should("have.value", "test");
     //  sets Target MRL from dropdown menu to value
-    cy.get("#target-mrl-select")
-      .select(`${value}`)
-      .should("have.value", `${value}`);
-
+    cy.get("#target-mrl-select").select(`${value}`).should("have.value", `${value}`);
     // finds Level Switching to be false
-    cy.get("#level-switching-select > option[value='false']").should(
-      "have.value",
-      "false"
-    );
-
+    cy.get("#level-switching-select > option[value='false']").should("have.value","false");
     // sets Level Switching to be on
     cy.get("#level-switching-select ").select("On");
-
     // finds and clicks start button
     cy.get("#assessment-start > .button-inner").click();
-
     // finds and validates MR Level to be ${value}
     cy.get(".desktop > #open-thread-popover").should($s => {
       expect($s).to.contain.text(`Current MR Level: ${value}`);
-      mrLevel = $s
-      .text()
-      .match(/\d+/g)[0];
+      mrLevel = $s.text().match(/\d+/g)[0];
     });
-
     // finds and validates Target MRL to be ${value}
     cy.get(".targets-scope > :nth-child(1)").should($p => {
       expect($p).to.contain.text(`Target MRL: ${value}`);
-
-      targetMrl = $p
-        .text()
-        .match(/\d+/g)[0]
-
-        expect(mrLevel).to.equal(targetMrl);
+      targetMrl = $p.text().match(/\d+/g)[0]
+      expect(mrLevel).to.equal(targetMrl);
     });
 
     });
