efdf6df840d69d721df460533149a47c230fbfb4
jcce <toshajo99@gmail.com>
2020-04-08 12:32:17 -0500

    Abstracted login code to a Command function

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,30 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add("login", (email, password) => {
+
+  // it is ok for the email to be visible in the Command Log
+  expect(email, "email was set").to.be.a("string").and.not.be.empty;
+  // but the password value should not be shown
+  if (typeof password !== "string" || !password) {
+    throw new Error("Missing password value, set using CYPRESS_password=...");
+  }
+
+  cy.get('input[name="emaial"]')
+    .type(email)
+    .should("have.value", email);
+
+
+  cy.get("[name=passwd]")
+    .type(password, { log: false })
+    .should(el$ => {
+      if (el$.val() !== password) {
+        throw new Error("Different value of typed password");
+      }
+    });
+
+  cy.get(".button").click();
+ })
 //
 //
 // -- This is a child command --
