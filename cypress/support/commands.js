793e77d8c2f2cbfba6674750511b28b435fc63b4
jcce <toshajo99@gmail.com>
2020-04-08 17:05:25 -0500

    Gave var more readable name. Appended password failure message. Removed redundant code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,18 +16,15 @@ Cypress.Commands.add("login", (email, password) => {
   expect(email, "email was set").to.be.a("string").and.not.be.empty;
   // but the password value should not be shown
   if (typeof password !== "string" || !password) {
-    throw new Error("Missing password value, set using CYPRESS_password=...");
+    throw new Error("Missing password value, set using CYPRESS_password= or in cypress env file...");
   }
 
-  cy.get('input[name="emaial"]')
-    .type(email)
-    .should("have.value", email);
-
+  cy.get('input[name="emaial"]').type(email);
 
   cy.get("[name=passwd]")
     .type(password, { log: false })
-    .should(el$ => {
-      if (el$.val() !== password) {
+    .should($password => {
+      if ($password.val() !== password) {
         throw new Error("Different value of typed password");
       }
     });
