17990297a5539b871f306d379566151389ae26f7
jcce <toshajo99@gmail.com>
2020-04-08 20:40:10 -0500

    Refactored code to more realable form

--- cypress/integration/users-guide-link.js
+++ cypress/integration/users-guide-link.js
@@ -1,39 +1,23 @@
 describe("A downloadable link for User's Guide", function() {
+  const email = Cypress.env("email");
+  const password = Cypress.env("password");
+  const baseUrl = Cypress.env("baseUrl");
+  const viewportWidth = Cypress.env("viewportWidth");
+  const viewportHeight = Cypress.env("viewportHeight");
+
+  const urlLink = "http://www.dodmrl.com/Users_Guide_2018_Version1.xlsm";
+  const helpButton =
+        "topbar.desktop > .toolbar-class > :nth-child(1) > .docent-header > .toolbar > .toolbar-content > .container-toolbar > .container-tbbuttons > #help-open-button > .button-inner";
+  const usersGuideButton = "#users-guide-button";
 
   beforeEach(() => {
-    const email = Cypress.env("email");
-    const password = Cypress.env("password");
-    const baseUrl = Cypress.env("baseUrl");
-
     cy.visit(baseUrl);
-
-    // it is ok for the email to be visible in the Command Log
-    expect(email, "email was set").to.be.a("string").and.not.be.empty;
-    // but the password value should not be shown
-    if (typeof password !== "string" || !password) {
-      throw new Error("Missing password value, set using CYPRESS_password=...");
-    }
-
-    cy.get('input[name="emaial"]')
-      .type(email)
-      .should("have.value", email);
-    cy.get("[name=passwd]")
-      .type(password, { log: false })
-      .should(el$ => {
-        if (el$.val() !== password) {
-          throw new Error("Different value of typed password");
-        }
-      });
-    cy.get(".button").click();
+    cy.login(email, password);
+    cy.viewport(viewportWidth, viewportHeight);
   });
 
   it("finds proper link for MRL Desktop", () => {
-    const helpButton =
-      "topbar.desktop > .toolbar-class > :nth-child(1) > .docent-header > .toolbar > .toolbar-content > .container-toolbar > .container-tbbuttons > #help-open-button > .button-inner";
-    const urlLink = "http://www.dodmrl.com/Users_Guide_2018_Version1.xlsm";
-
-    cy.location("pathname").should("eq", "/");
     cy.get(helpButton).click();
-    cy.get("#users-guide-button").should("have.attr","href",urlLink);
+    cy.get(usersGuideButton).should("have.attr", "href", urlLink);
   });
 });
