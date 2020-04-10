5e9b3148b3fe5fee7ad18a230b91d033adf419be
jcce <toshajo99@gmail.com>
2020-04-09 20:37:14 -0500

    Changed value to mrLevel

--- cypress/integration/top-bar.js
+++ cypress/integration/top-bar.js
@@ -4,7 +4,7 @@ describe("determine MR Level is same as Target MRL", function() {
   const baseUrl = Cypress.env("baseUrl");
   const viewportWidth = Cypress.env("viewportWidth");
   const viewportHeight = Cypress.env("viewportHeight");
-  const value = 9;
+  const mrLevel = 9;
 
   // New assessment vars
   const startNewButton = ".buttons > :nth-child(1) > .button-inner";
@@ -12,14 +12,13 @@ describe("determine MR Level is same as Target MRL", function() {
   const mrlDropdown = "#target-mrl-select";
   const levelSwitchDropdown = "#level-switching-select";
   const mainAssessmentStartButton = "#assessment-start > .button-inner";
-
   const currentMrlLevel = ".desktop > #open-thread-popover";
   const targetMrlLevel = ".targets-scope > :nth-child(1)";
 
   const beginNewAssesment = () => {
     cy.get(startNewButton).click();
     cy.get(assessmentNameField).type("test");
-    cy.get(mrlDropdown).select(`${value}`);
+    cy.get(mrlDropdown).select(`${mrLevel}`);
     cy.get(levelSwitchDropdown).select("On");
     cy.get(mainAssessmentStartButton).click();
   };
@@ -38,12 +37,12 @@ describe("determine MR Level is same as Target MRL", function() {
     beginNewAssesment();
 
     cy.get(currentMrlLevel).should($element => {
-      expect($element).to.contain.text(`Current MR Level: ${value}`);
+      expect($element).to.contain.text(`Current MR Level: ${mrLevel}`);
       mrLevel = $element.text().match(findElementNumberRegex)[0];
     });
 
     cy.get(targetMrlLevel).should(($target) => {
-      expect($target).to.contain.text(`Target MRL: ${value}`);
+      expect($target).to.contain.text(`Target MRL: ${mrLevel}`);
       targetMrl = $target.text().match(findElementNumberRegex)[0];
       expect(mrLevel).to.equal(targetMrl);
     });
