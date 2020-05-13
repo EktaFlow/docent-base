de64307dd15b9b5032aa05ca7eb5cf3ea95bb748
jcce <toshajo99@gmail.com>
2020-05-13 15:08:12 -0500

    Separate email and pw from in login test

--- cypress/integration/login-error-validation-test.js
+++ cypress/integration/login-error-validation-test.js
@@ -26,24 +26,31 @@ describe("Error validations on Login page should be visible ", function () {
     cy.viewport(viewportWidth, viewportHeight);
   });
 
-  it("displays proper validation erros if submitted form is blank ", () => {
+  it("displays proper email validation erros if submitted form is blank ", () => {
     cy.get(submitButton).click();
     cy.get(emailBlankId).should(($error) => {
       expect($error).to.contain.text(emailBlankMessage);
     });
+  });
+
+  it("displays proper email validation errors if submitted form is partial ", () => {
+    cy.get(nameInput).type(emailInvalid);
+    cy.get(submitButton).click();
+    cy.get(emailInvalidId).should(($error) => {
+      expect($error).to.contain.text(emailInvalidMessage);
+    });
+  });
+
+  it("displays proper password validation erros if submitted form is blank ", () => {
+    cy.get(submitButton).click();
     cy.get(passwordBlankId).should(($error) => {
       expect($error).to.contain.text(passwordBlankMessage);
     });
   });
 
-  it("displays proper validation errors if submitted form is partial ", () => {
-    cy.get(nameInput).type(emailInvalid);
+  it("displays proper password validation errors if submitted form is partial ", () => {
     cy.get(passwordInput).type(passwordInvalid);
     cy.get(submitButton).click();
-
-    cy.get(emailInvalidId).should(($error) => {
-      expect($error).to.contain.text(emailInvalidMessage);
-    });
     cy.get(passwordInvalidId).should(($error) => {
       expect($error).to.contain.text(passwordInvalidMessage);
     });
